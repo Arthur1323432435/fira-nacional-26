2f7969ffb36b8b537144df65ff06961060a735d7
Arthur1323432435 <abreu.arthur@discente.ufma.br>
2026-08-08 18:25:49 -0300

    push

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,18 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        ligaUniversitaria: resolve(__dirname, 'liga_universitaria.html'),
-        ligaKids: resolve(__dirname, 'liga_kids.html'),
-        ligaJuvenil: resolve(__dirname, 'liga_juvenil.html'),
+        main: resolve(import.meta.dirname, 'index.html'),
+        ligaUniversitaria: resolve(import.meta.dirname, 'liga_universitaria.html'),
+        ligaKids: resolve(import.meta.dirname, 'liga_kids.html'),
+        ligaJuvenil: resolve(import.meta.dirname, 'liga_juvenil.html'),
 
-        etapasEstaduais: resolve(__dirname, 'etapas-estaduais.html'),
-        etapaNacionail: resolve(__dirname, 'etapa_nacional.html'),
-        etapaMundial: resolve(__dirname, 'etapa_mundial.html'),
+        etapasEstaduais: resolve(import.meta.dirname, 'etapas-estaduais.html'),
+        etapaNacional: resolve(import.meta.dirname, 'etapa_nacional.html'),
+        etapaMundial: resolve(import.meta.dirname, 'etapa_mundial.html'),
 
-        loja: resolve(__dirname, 'loja.html'),
-        produto: resolve(__dirname, 'produto.html'),
-        resultados: resolve(__dirname, 'resultados.html'),
+        loja: resolve(import.meta.dirname, 'loja.html'),
+        produto: resolve(import.meta.dirname, 'produto.html'),
+        resultados: resolve(import.meta.dirname, 'resultados.html'),
       },
     },
   },
